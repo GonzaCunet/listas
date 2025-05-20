5390cf0977b9fc45c111806cb09eba804e6b7307
Gonza <gonza.cunet@outlook.com>
2025-05-20 13:15:30 -0300

    second commit

--- pages/api/listas/index.ts
+++ pages/api/listas/index.ts
@@ -1,15 +1,31 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { parse } from "path";
 
 function getLista() {
-  const arr = Array.from(Array(10).keys());
+  const arr = Array.from(Array(1000).keys());
   return arr.map((valor, indice) => {
     return { nombre: valor };
   });
 }
 
+function getOffsetAndLimitFromReq(req: NextApiRequest, maxLimit, maxOffset) {
+  const queryLimit = parseInt(req.query.limit as string);
+  const queryOffset = parseInt(req.query.offset as string);
+  const limit = queryLimit <= maxLimit ? queryLimit : maxLimit;
+  const offset = queryOffset < maxOffset ? queryOffset : 0;
+  return { limit, offset };
+}
+
 export default function (req: NextApiRequest, res: NextApiResponse) {
   const lista = getLista();
+  const { offset, limit } = getOffsetAndLimitFromReq(req, 100, lista.length);
+  const sliced = lista.slice(offset, offset + limit);
   res.send({
-    lista,
+    results: sliced,
+    pagination: {
+      offset,
+      limit,
+      total: lista.length,
+    },
   });
 }
